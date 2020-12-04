2cac9a150f903ddce84538bf4c99fe86d6cc460c
LT <leetoplayaz@yandex.ru>
2020-12-04 10:55:32 +0300

    cashItems

--- unitpay/unitpay.php
+++ unitpay/unitpay.php
@@ -33,26 +33,136 @@ class plgVmPaymentUnitpay extends vmPSPlugin
         $domain = $method->domain;
         $public_key = $method->public_key;
         $secret_key = $method->secret_key;
-        $sum = $order['details']['BT']->order_total;
+        $sum = number_format($order['details']['BT']->order_total, 2, '.', '');
+
+        $this->getPaymentCurrency($method);
+
+        //get currency code
+        $q = 'SELECT `currency_code_3` FROM `#__virtuemart_currencies` WHERE `virtuemart_currency_id`="'.
+                $method->payment_currency.'" ';
+        $db = JFactory::getDBO();
+        $db->setQuery($q);
+        $currencyCode = $db->loadResult();
+
+        $totalInPaymentCurrency = vmPSPlugin::getAmountInCurrency(
+                $order['details']['BT']->order_total,$method->payment_currency
+        );
+        $cartCurrency = CurrencyDisplay::getInstance($cart->pricesCurrency);
+
         $account = $order['details']['BT']->virtuemart_order_id;
         $desc = 'Оплата по заказу №' . $order['details']['BT']->order_number;
         $signature = hash('sha256', join('{up}', array(
             $account,
+            $currencyCode,
             $desc,
             $sum,
             $secret_key
         )));
 
+        $address = isset($order['details']['ST']) ? $order['details']['ST'] : $order['details']['BT'];
+
         $html = '<form name="unitpay" action="https://'.$domain.'/pay/' . $public_key . '" method="get">';
         $html .= '<input type="hidden" name="sum" value="' . $sum . '">';
         $html .= '<input type="hidden" name="account" value="' . $account . '">';
+        $html .= '<input type="hidden" name="currency" value="' . $currencyCode . '">';
         $html .= '<input type="hidden" name="desc" value="' . $desc . '">';
         $html .= '<input type="hidden" name="signature" value="' . $signature . '">';
+        $html .= '<input type="hidden" name="customerPhone" value="' . preg_replace('/\D/', '', $address->phone_1) . '">';
+        $html .= '<input type="hidden" name="customerEmail" value="' . $address->email . '">';
+        $html .= '<input type="hidden" name="cashItems" value="' . $this->cashItems($order, $currencyCode) . '">';
         $html .= '</form>';
         $html .= '<script type="text/javascript">';
         $html .= 'document.forms.unitpay.submit();';
         $html .= '</script>';
+
         return $this->processConfirmedOrderPaymentResponse(true, $cart, $order, $html, $this->renderPluginName($method, $order), 'P');
+    }
+
+    function cashItems($order, $orderCurrencyCode) {
+        $billing = $order['details']['BT'];
+
+        $items = array();
+
+        foreach ($order['items'] as $item) {
+            $prices = $item->allPrices[$item->selectedPrice];
+            $db = JFactory::getDBO();
+
+            $q = 'SELECT `currency_code_3` FROM `#__virtuemart_currencies` WHERE `virtuemart_currency_id`="'.$prices["payment_currency"].'" ';
+            $db->setQuery($q);
+            $currencyCode = $db->loadResult();
+
+            $q = 'SELECT `calc_value` FROM `#__virtuemart_calcs` WHERE `virtuemart_calc_id`="'.$prices["product_tax_id"].'" ';
+            $db->setQuery($q);
+            $tax = $db->loadResult();
+
+            $taxRate = $item->product_tax*100;
+
+            $items[] = array(
+                'name' => mb_strcut($item->order_item_name, 0, 63),
+                'count' => round($item->product_quantity),
+                'currency' => $currencyCode ? $currencyCode : $orderCurrencyCode,
+                'price' => $item->product_subtotal_with_tax/round($item->product_quantity),
+                'nds' => $tax ? $this->taxRates($tax) : "none",
+                'type' => 'commodity',
+            );
+        }
+
+        if(!empty($billing)) {
+            $shipment_cost = 0;
+
+            if(!empty($billing->order_shipment))
+                $shipment_cost += $billing->order_shipment;
+
+            if(!empty($billing->order_shipment_tax))
+                $shipment_cost += $billing->order_shipment_tax;
+
+            if($shipment_cost > 0) {
+                $tax = array_shift(json_decode($billing->order_billTax, true));
+
+                $items[] = array(
+                    'name' => 'Услуги доставки',
+                    'count' => 1,
+                    'price' => number_format($shipment_cost, 2, '.', ''),
+                    'currency' => $orderCurrencyCode,
+                    'nds' => isset($tax->calc_value) ? $this->taxRates($tax->calc_value) : "none",
+                    'type' => 'service',
+                );
+            }
+        }
+
+        return base64_encode(json_encode($items));
+    }
+
+    function taxRates($rate){
+        switch (intval($rate)){
+            case 10:
+                $vat = 'vat10';
+                break;
+            case 20:
+                $vat = 'vat20';
+                break;
+            case 0:
+                $vat = 'vat0';
+                break;
+            default:
+                $vat = 'none';
+        }
+
+        return $vat;
+    }
+
+    function plgVmgetPaymentCurrency ($virtuemart_paymentmethod_id, &$paymentCurrencyId) {
+
+        if (!($method = $this->getVmPluginMethod ($virtuemart_paymentmethod_id))) {
+            return NULL; // Another method was selected, do nothing
+        }
+        if (!$this->selectedThisElement ($method->payment_element)) {
+            return FALSE;
+        }
+        $this->getPaymentCurrency ($method);
+
+        $paymentCurrencyId = $method->payment_currency;
+        return;
     }
 
     function plgVmDeclarePluginParamsPayment($name, $id, &$data)
@@ -190,15 +300,17 @@ class plgVmPaymentUnitpay extends vmPSPlugin
         $currenciesModel     = VmModel::getModel('currency');
         $currency = $currenciesModel->getCurrency($order_details->order_currency);
 
+        $total = number_format($order_details->order_total, 2, ".", "");
+
         if (empty($order['details'])){
             $result = array('error' =>
                 array('message' => 'заказа не существует')
             );
-        }elseif ((float)$order_details->order_total != (float)$params['orderSum']) {
+        }elseif ((float) $total != (float) number_format($params['orderSum'], 2, ".", "")) {
             $result = array('error' =>
                 array('message' => 'не совпадает сумма заказа')
             );
-        }elseif ($currency->currency_code_3 != $params['orderCurrency']) {
+        } elseif ($currency->currency_code_3 != $params['orderCurrency']) {
             $result = array('error' =>
                 array('message' => 'не совпадает валюта заказа')
             );
@@ -223,11 +335,13 @@ class plgVmPaymentUnitpay extends vmPSPlugin
         $currenciesModel     = VmModel::getModel('currency');
         $currency = $currenciesModel->getCurrency($order_details->order_currency);
 
+        $total = number_format($order_details->order_total, 2, ".", "");
+
         if (empty($order['details'])){
             $result = array('error' =>
                 array('message' => 'заказа не существует')
             );
-        }elseif ((float)$order_details->order_total != (float)$params['orderSum']) {
+        }elseif ((float) $total != (float) number_format($params['orderSum'], 2, ".", "")) {
             $result = array('error' =>
                 array('message' => 'не совпадает сумма заказа')
             );
